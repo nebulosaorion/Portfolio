922122e55c8729c2c326e20bb0808bf3401bbe20
Miriã Evangelista <mimi.mucra@gmail.com>
2026-05-16 22:20:50 -0300

    Corrige abertura de links no web

--- lib/util/abridor_link.dart
+++ lib/util/abridor_link.dart
@@ -2,7 +2,9 @@ import 'package:url_launcher/url_launcher.dart';
 
 Future<void> abrirLink(String valor) async {
   final uri = Uri.parse(valor);
-  if (await canLaunchUrl(uri)) {
-    await launchUrl(uri, webOnlyWindowName: '_blank');
-  }
+  await launchUrl(
+    uri,
+    mode: LaunchMode.platformDefault,
+    webOnlyWindowName: '_blank',
+  );
 }
