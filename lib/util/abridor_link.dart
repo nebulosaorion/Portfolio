81501d15fb1229f6355454623b95023476ab0d8e
Miriã Evangelista <mimi.mucra@gmail.com>
2026-05-16 21:07:36 -0300

    Atualização

--- lib/util/abridor_link.dart
+++ lib/util/abridor_link.dart
@@ -3,6 +3,6 @@ import 'package:url_launcher/url_launcher.dart';
 Future<void> abrirLink(String valor) async {
   final uri = Uri.parse(valor);
   if (await canLaunchUrl(uri)) {
-    await launchUrl(uri);
+    await launchUrl(uri, webOnlyWindowName: '_blank');
   }
 }
